2cea3732533b29c0e64ff5169acfc9b92058718b
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-11-16 19:16:39 -0300

    Update main.js

--- aluraFlix2/src/main.js
+++ aluraFlix2/src/main.js
@@ -1,11 +1,11 @@
 function adicionarFilme() {
     let filmeFavorito = document.getElementById('filme').value;
-    let elementoListaFilmes = document.getElementById('listaFilmes');
+    let elementFilmes = document.getElementById('listaFilmes');
     let inputFilme = document.getElementById('filme');
     let urlValida = /^(http|https):\/\/[^ "]+$/.test(filmeFavorito);
   
     if (urlValida) {
-      elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>';
+      elementFilmes.innerHTML = elementFilmes.innerHTML + '<img src=' + filmeFavorito + '>';
       inputFilme.value = '';
     } else {
       inputFilme.value = '';
